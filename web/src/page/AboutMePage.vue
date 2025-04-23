f3c0243914aab5b27714d8646f1f54929a3cdcb1
mugimugi6262 <momoiro93ando@gmail.com>
2025-04-23 17:26:25 +0900

    add：【画面】レイアウト(PawLayout.vue)の追加

---
+++ web/src/page/AboutMePage.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import api from '@/api/api'
+import { useRouter } from 'vue-router'
+
+
+/** ルーター */
+const router = useRouter()
+
+
+/** リアクティブデータの定義 */
+
+/** メソッドの定義 */
+
+</script>
