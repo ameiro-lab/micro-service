3928a702f94a4287605e77e355934beccffa695f
mugimugi6262 <momoiro93ando@gmail.com>
2025-04-23 17:44:26 +0900

    fix：フロントエンド調整

--- web/src/App.vue
+++ web/src/App.vue
@@ -12,4 +12,19 @@
 * {
   font-family: 'Poppins', sans-serif !important;
 }
+/* 25対75のレイアウト */
+.layout-25-75 {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+}
+.layout-25-75 .content-25 {
+  width: 25%;
+  min-width: 150px;
+  max-width: 25%;
+}
+.layout-25-75 .content-75 {
+  width: 75%;
+}
 </style>
